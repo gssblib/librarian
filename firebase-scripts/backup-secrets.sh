3b9453e22cab3fd90cd7c263ac6a938177e1e772
Kai Bolay <kbolay@google.com>
2022-09-17 11:57:03 -0400

    update secret paths

--- firebase-scripts/backup-secrets.sh
+++ firebase-scripts/backup-secrets.sh
@@ -7,8 +7,7 @@ SECRETS_BACKUP_FILE=/tmp/gssb-firebase-secrets.tar.gz
 
 cd ${BASEDIR}
 tar cfz ${SECRETS_BACKUP_FILE} \
-    provision/grains \
-    server/config \
+    server/config/*.json \
     server/.env \
     client/src/environments
 ls -al ${SECRETS_BACKUP_FILE}
