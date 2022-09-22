e7558c8f90bad6b8ad90a0b3f2deefbefa98f3f3
Stephan Richter <stephan.richter@gmail.com>
2022-09-22 16:05:49 -0400

    Newly required setup to load dumps.

--- sql/bootstrap.sql
+++ sql/bootstrap.sql
@@ -1,3 +1,6 @@
 CREATE USER IF NOT EXISTS 'gssb'@'localhost' IDENTIFIED BY 'gssblib';
 CREATE DATABASE IF NOT EXISTS spils;
 GRANT ALL PRIVILEGES ON spils.* TO 'gssb'@'localhost' WITH GRANT OPTION;
+-- Neeeded to load data dumps.
+GRANT SYSTEM_VARIABLES_ADMIN ON *.* TO 'gssb'@'localhost';
+FLUSH PRIVILEGES;
