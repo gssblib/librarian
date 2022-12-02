2640243d79327dd70078e4909badbdaf2309e216
Kai Bolay <kbolay@google.com>
2022-12-02 01:36:47 -0500

    Increase timeout to five minutes.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
 import {app} from "./server";
 import {onRequest} from "firebase-functions/v2/https";
+import {setGlobalOptions} from "firebase-functions/v2";
+
+setGlobalOptions({ timeoutSeconds: 5*60 });
 
 exports.api = onRequest(app);
